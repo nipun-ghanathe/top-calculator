d5023a57951a3d1f1f1f78c3432f042e1410a44d
nipun <nipuncod@gmail.com>
2026-03-19 15:40:00 +0530

    Add basic functionality

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
 const operations = {
-  "+": add,
-  "-": subtract,
-  "*": multiply,
-  "/": divide,
-};
-
-function operator(op, a, b) {
-  return operations[op](a, b);
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
 }
+operations['×'] = operations['*']
+operations['÷'] = operations['/']
+
+let operandA = null
+let operandB = null
+let operator = null
+let clearDisplay = null
+
+const display = document.querySelector('.display')
+
+const nums = document.querySelectorAll('.num:not(.unary-op)')
+for (let num of nums) {
+  num.addEventListener('click', () => {
+    if (clearDisplay) {
+      display.textContent = ''
+      clearDisplay = false
+    }
+    const value = num.textContent
+    if (value !== '.' || !display.textContent.includes('.')) {
+      display.textContent += value
+    }
+  })
+}
+
+const opBtns = document.querySelectorAll('.op')
+for (let opBtn of opBtns) {
+  const op = opBtn.textContent
+
+  opBtn.addEventListener('click', () => {
+    if (!operator) {
+      operandA = +display.textContent
+      operator = op
+      clearDisplay = true
+    } else {
+      operandB = +display.textContent
+      const result = operations[operator](operandA, operandB)
+      display.textContent = result
+      operandB = null
+      if (opBtn.textContent !== '=') {
+        operandA = result
+        operator = op
+        clearDisplay = true
+      } else {
+        operandA = null
+        operator = null
+      }
+    }
+  })
+}
+
+const clearBtn = document.querySelector('.clear')
+clearBtn.addEventListener('click', () => {
+  display.textContent = ''
+})
