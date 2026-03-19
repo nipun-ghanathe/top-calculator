34e49ca92e8611010580e7c9e4d1c1b8e8f0e3ea
nipun <nipuncod@gmail.com>
2026-03-19 18:10:22 +0530

    Add keyboard support

--- script.js
+++ script.js
@@ -10,9 +10,51 @@ operations['÷'] = operations['/']
 let operandA = null
 let operandB = null
 let operator = null
-let clearDisplay = null
+let clearDisplay = false
+
+function clickButton(eleId) {
+  document.querySelector(`#${eleId}`).click()
+}
+
+const otherAllowedKeys = ['Backspace', 'Enter']
 
 const display = document.querySelector('.display')
+
+display.addEventListener('keydown', (e) => {
+  if (['Backspace', 'Delete'].includes(e.key)) return
+
+  e.preventDefault()
+
+  if (/\d/.test(e.key)) {
+    clickButton(`num-${e.key}`)
+    return
+  }
+
+  switch (e.key) {
+    case '.':
+      clickButton('num-decimal')
+      break
+    case 'Escape':
+      clickButton('clear-entry')
+      break
+    case 'Enter':
+    case '=':
+      clickButton('equal')
+      break
+    case '+':
+      clickButton('add')
+      break
+    case '-':
+      clickButton('sub')
+      break
+    case '*':
+      clickButton('mul')
+      break
+    case '/':
+      clickButton('div')
+      break
+  }
+})
 
 const nums = document.querySelectorAll('.num:not(#sign-change)')
 for (let num of nums) {
@@ -34,14 +76,16 @@ for (let opBtn of opBtns) {
 
   opBtn.addEventListener('click', () => {
     if (!operator) {
-      operandA = +display.value
+      operandA = display.value ? +display.value : 0
       operator = op !== '=' ? op : null
       clearDisplay = true
     } else {
       if (clearDisplay && op !== '=') {
+        operator = op
         return
       }
       operandB = +display.value
+      if (!operations[operator]) return
       const result = operations[operator](operandA, operandB)
       display.value = result
       clearDisplay = true
@@ -59,6 +103,7 @@ for (let opBtn of opBtns) {
 
 const signChangeBtn = document.querySelector('#sign-change')
 signChangeBtn.addEventListener('click', () => {
+  if (!display.value) return
   if (display.value.startsWith('-')) {
     display.value = display.value.slice(1)
   } else {
